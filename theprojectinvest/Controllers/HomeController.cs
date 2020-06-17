a34597caf044be99dde959bb6d508d00ea7cec55
Hakan Kalkan <hakan.kalkan@bil.omu.edu.tr>
2020-06-17 15:08:16 +0300

    firstLoad

---
+++ theprojectinvest/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace theprojectinvest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult AnaSayfa()
+        {
+            return View();
+        }
+        public ActionResult Hizmetlerimiz()
+        {
+            return View();
+            
+        }
+        public ActionResult Proje()
+        {
+            return View();
+        }
+        public ActionResult Yatirim()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public ActionResult Sektorler()
+        {
+            return View();
+        }
+
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ theprojectinvest/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="theprojectinvest.MvcApplication" Language="C#" %>
